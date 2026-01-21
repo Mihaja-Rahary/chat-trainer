1103921ef9a007a6078620d4cbbf6d782cc02583
MellowMih <127590992+Mihaja-Rahary@users.noreply.github.com>
2026-01-21 15:00:06 +0100

    Add check to create users.json if missing
    Ensure users.json file is created if it does not exist.

--- api/users.js
+++ api/users.js
@@ -2,9 +2,10 @@ import fs from 'fs';
 import path from 'path';
 const usersPath = path.join(process.cwd(),'data/users.json');
 
+// Créer fichier vide si inexistant
+if (!fs.existsSync(usersPath)) fs.writeFileSync(usersPath,'[]','utf8');
+
 export default function handler(req,res){
   const users = JSON.parse(fs.readFileSync(usersPath,'utf8'));
-  const role = req.query.role || null;
-  if(role) res.status(200).json(users.filter(u=>u.role==="chatter" || role==="superadmin"));
-  else res.status(200).json(users);
+  res.status(200).json(users);
 }
